e5f4882033a6741dbc9701709025b48888b46bc4
Zachary Sloss <zachary.sloss@gmail.com>
2016-01-05 17:31:04 +0800

    Ensure that vim plugin directory exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,7 @@ npm config set prefix '~/npm-global'
 echo 'export PATH=~/npm-global/bin:$PATH' >> ~/.profile
 source ~/.profile
 
+mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/mxw/vim-jsx.git
 git clone https://github.com/pangloss/vim-javascript.git
